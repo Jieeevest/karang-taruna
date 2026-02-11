8be2837f1d0e4ed9e24fc68ac26d371b2bf01633
Iqbaalaji <ajieeevest@gmail.com>
2026-02-12 02:28:33 +0700

    feat: add competition registrations navigation link and update CMS layout paths and dashboard routes

--- resources/views/cms/layouts/app.blade.php
+++ resources/views/cms/layouts/app.blade.php
@@ -124,6 +124,17 @@
                     </a>
                     @endif
                     
+                    <!-- Competition Registrations - Ketua, Admin Data, & Anggota -->
+                    @if(auth()->user()->isKetua() || auth()->user()->isAdmin() || auth()->user()->isAnggota())
+                    <a href="{{ route('cms.competition-registrations.index') }}" class="flex items-center p-3 text-gray-700 rounded-lg menu-item @if(request()->routeIs('cms.competition-registrations.*')) menu-item-active @endif">
+                        <svg class="w-5 h-5 mr-4" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
+                            <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="font-medium">Pendaftaran Lomba</span>
+                    </a>
+                    @endif
+                    
                     <!-- Documentation - Ketua, Admin Data, & Anggota -->
                     @if(auth()->user()->isKetua() || auth()->user()->isAdmin() || auth()->user()->isAnggota())
                     <a href="{{ route('cms.documentation.index') }}" class="flex items-center p-3 text-gray-700 rounded-lg menu-item @if(request()->routeIs('cms.documentation.*')) menu-item-active @endif">
